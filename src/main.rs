9ae31fb8a107f436cc107d52fd6e9ed6d446c1c4
aingbager <aingbager55@gmail.com>
2023-10-12 18:39:56 +0700

    add shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::{
+    io::{self, Write},
+    process, thread, time,
+};
+
+use rand::Rng;
+
+fn acak() -> i32 {
+    rand::thread_rng().gen_range(0..250)
+}
+
+fn shutdown() {
+    process::Command::new("systemctl")
+        .arg("poweroff")
+        .output()
+        .expect("shutdown error");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_user = String::new();
+    print!("masukan angka target: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input_user).unwrap();
+
+    let input_user: i32 = input_user.trim().parse().expect("harus angka");
+
+    println!("target pencarian: {}", input_user);
+
+    loop {
+        let target = acak();
+        println!("{}", target);
+        thread::sleep(time::Duration::from_secs(2));
+
+        if target == input_user {
+            println!("komputer dimatikan....");
+            shutdown();
+        }
+    }
 }
